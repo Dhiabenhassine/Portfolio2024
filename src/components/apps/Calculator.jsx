68b0fd715490e9a0a1f249353396983c58f287b5
Dhiabenhassine <Dhiabenhssin123@gmail.com>
2024-08-24 18:50:51 +0100

    version 0.0.2

--- src/components/apps/Calculator.jsx
+++ src/components/apps/Calculator.jsx
@@ -1,264 +1,107 @@
-import React, { useState, useRef } from "react";
-import Draggable from "react-draggable";
+import React,{useState} from 'react';
+import './calculator.css'; 
+import Draggable from 'react-draggable';
+import { evaluate } from 'mathjs';
 
-const Calculator = ({ isAppOpen, toggleCalculator }) => {
-  const calculatorRef = useRef(null);
-  const [display, setDisplay] = useState("");
-  const [showResult, setShowResult] = useState(" Am I Right?");
-  const [submit, setSubmit] = useState(false);
-  const [clickCount, setClickCount] = useState(0);
+function Calculator({ isAppOpen, toggleCalculator }) {
+  const [input, setInput] = useState('');
 
-  const appendToDisplay = (value) => {
-    setDisplay((prevDisplay) => prevDisplay + value);
+  const handleButtonClick = (value) => {
+    setInput((prev) => prev + value);
   };
 
-  const calculate = () => {
+  const handleCalculate = () => {
     try {
-      const result = eval(display);
-      setClickCount((prevClickCount) => prevClickCount + 1);
-
-      if (clickCount === 0 || clickCount === 4) {
-        setDisplay("Hello World");
-        setClickCount(1);
-      } else {
-        if (result !== undefined && !isNaN(result)) {
-          setDisplay(result.toString());
-          setSubmit(true);
-        } else {
-          setDisplay("Enter Something Stoopid");
-          setTimeout(() => {
-            setDisplay("");
-          }, 1000);
-        }
-      }
+      const result = evaluate(input); 
+      setInput(result.toString());
     } catch (error) {
-      setDisplay("Error");
-      setTimeout(() => {
-        setDisplay("");
-      }, 1000);
+      setInput('Error');
     }
   };
 
-  const clearDisplay = () => {
-    setDisplay("");
-    setShowResult(" Am I Right?");
-    setSubmit(false);
-  };
-
-  const handleYesClick = () => {
-    setShowResult("Too Easy😎");
-    setTimeout(() => {
-      setSubmit(false);
-      setShowResult(" Am I Right?");
-      setDisplay("");
-    }, 3000);
-  };
-
-  const handleNoClick = () => {
-    setShowResult("BRUH 💀");
-    setTimeout(() => {
-      setSubmit(false);
-      setShowResult(" Am I Right?");
-      setDisplay("");
-    }, 3000);
-  };
-
-  const screenWidth = window.innerWidth;
-  const screenHeight = window.innerHeight;
-  const bounds = {
-    left: 0,
-    top: 0,
-    right: screenWidth - 544,
-    bottom: screenHeight - 800,
+  const handleClear = () => {
+    setInput('');
   };
 
   return (
-    <div
-      className={`${
-        isAppOpen ? "" : "hidden"
-      } z-30 w-full h-screen pointer-events-none absolute`}
-    >
-      <Draggable handle=".title-bar" nodeRef={calculatorRef} bounds={bounds}>
-        <div
-          ref={calculatorRef}
-          className="window bg-black w-[34em] h-[50em] rounded-xl overflow-hidden border-neutral-700 border-[1.5px] pointer-events-auto"
-        >
-          <div className="title-bar">
-            <div className="text-white h-9 flex justify-between select-none">
-              <div className="m-1 ml-4 font-normal">Calculator</div>
-              <div className="flex">
-                <div
-                  className="material-symbols-outlined hover:bg-neutral-800 mb-2 w-11 flex justify-center items-center text-xl"
-                  onClick={toggleCalculator}
-                >
-                  minimize
-                </div>
-                <div className="material-symbols-outlined hover:bg-neutral-800 mb-2 w-11 flex justify-center items-center text-sm">
-                  check_box_outline_blank
-                </div>
-                <div
-                  className="material-symbols-outlined hover:bg-red-700 mb-2 w-12 flex justify-center items-center text-xl"
-                  onClick={toggleCalculator}
-                >
-                  close
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="content text-white select-none text-center flex justify-center">
-            <div className="top-[10px] bg-neutral-900 mx-auto p-20 shadow-lg text-white h-screen">
-              <input
-                type="text"
-                value={display}
-                className="w-full mb-10 px-4 py-3 text-3xl rounded-lg bg-transparent shadow-inner text-right"
-                placeholder="0"
-                disabled
-              />
-              <div className="grid grid-cols-4 gap-3 text-2xl font-light">
-                <div
-                  className={`text-white text-center text-4l col-span-2 ${
-                    submit ? "" : "hidden"
-                  }`}
-                >
-                  {showResult}
-                </div>
-                <button
-                  className={`btn btn-success text-white p-3 ${
-                    submit ? "" : "hidden"
-                  }`}
-                  onClick={handleYesClick}
-                >
-                  YES
-                </button>
-                <button
-                  className={`btn btn-error text-white p-3 ${
-                    submit ? "" : "hidden"
-                  }`}
-                  onClick={handleNoClick}
-                >
-                  NO
-                </button>
-                <button
-                  onClick={clearDisplay}
-                  className="p-6 text-center bg-gray-300 rounded-full hover:bg-opacity-60 focus:outline-none bg-opacity-65"
-                >
-                  AC
-                </button>
-                <button
-                  onClick={() => appendToDisplay("*2")}
-                  className="p-6 text-center bg-gray-300 rounded-full hover:bg-opacity-60 focus:outline-none bg-opacity-65"
-                >
-                  x2
-                </button>
-                <button
-                  onClick={() => appendToDisplay("%")}
-                  className="p-6 text-center bg-gray-300 rounded-full hover:bg-opacity-60 focus:outline-none bg-opacity-65"
-                >
-                  %
-                </button>
-                <button
-                  onClick={() => appendToDisplay("/")}
-                  className="p-6 text-center bg-yellow-600 rounded-full hover:bg-opacity-60 focus:outline-none"
-                >
-                  /
-                </button>
-                <button
-                  onClick={() => appendToDisplay("7")}
-                  className="p-6 text-center bg-neutral-600 rounded-full hover:bg-opacity-60 focus:outline-none"
-                >
-                  7
-                </button>
-                <button
-                  onClick={() => appendToDisplay("8")}
-                  className="p-6 text-center bg-neutral-600 rounded-full hover:bg-opacity-60 focus:outline-none"
-                >
-                  8
-                </button>
-                <button
-                  onClick={() => appendToDisplay("9")}
-                  className="p-6 text-center bg-neutral-600 rounded-full hover:bg-opacity-60 focus:outline-none"
-                >
-                  9
-                </button>
-                <button
-                  onClick={() => appendToDisplay("*")}
-                  className="p-6 text-center bg-yellow-600 rounded-full hover:bg-opacity-60 focus:outline-none"
-                >
-                  x
-                </button>
-                <button
-                  onClick={() => appendToDisplay("4")}
-                  className="p-6 text-center bg-neutral-600 rounded-full hover:bg-opacity-60 focus:outline-none"
-                >
-                  4
-                </button>
-                <button
-                  onClick={() => appendToDisplay("5")}
-                  className="p-6 text-center bg-neutral-600 rounded-full hover:bg-opacity-60 focus:outline-none"
-                >
-                  5
-                </button>
-                <button
-                  onClick={() => appendToDisplay("6")}
-                  className="p-6 text-center bg-neutral-600 rounded-full hover:bg-opacity-60 focus:outline-none"
-                >
-                  6
-                </button>
-                <button
-                  onClick={() => appendToDisplay("-")}
-                  className="p-6 text-center bg-yellow-600 rounded-full hover:bg-opacity-60 focus:outline-none"
-                >
-                  -
-                </button>
-                <button
-                  onClick={() => appendToDisplay("1")}
-                  className="p-6 text-center bg-neutral-600 rounded-full hover:bg-opacity-60 focus:outline-none"
-                >
-                  1
-                </button>
-                <button
-                  onClick={() => appendToDisplay("2")}
-                  className="p-6 text-center bg-neutral-600 rounded-full hover:bg-opacity-60 focus:outline-none"
-                >
-                  2
-                </button>
-                <button
-                  onClick={() => appendToDisplay("3")}
-                  className="p-6 text-center bg-neutral-600 rounded-full hover:bg-opacity-60 focus:outline-none"
-                >
-                  3
-                </button>
-                <button
-                  onClick={() => appendToDisplay("+")}
-                  className="p-6 text-center bg-yellow-600 rounded-full hover:bg-opacity-60 focus:outline-none"
-                >
-                  +
-                </button>
-                <button
-                  onClick={() => appendToDisplay("0")}
-                  className="p-6 text-center bg-neutral-600 rounded-full hover:bg-opacity-60 focus:outline-none col-span-2"
-                >
-                  0
-                </button>
-                <button
-                  onClick={() => appendToDisplay(".")}
-                  className="p-6 text-center bg-neutral-600 rounded-full hover:bg-opacity-60 focus:outline-none"
-                >
-                  .
-                </button>
-                <button
-                  onClick={calculate}
-                  className="p-6 text-center bg-yellow-600 rounded-full hover:bg-opacity-60 focus:outline-none"
-                >
-                  =
-                </button>
-              </div>
-            </div>
+    isAppOpen && (
+      <Draggable>
+        <div style={styles.calculator}>
+          <div onClick={toggleCalculator} style={styles.closeButton}>X</div>
+          <input 
+            type="text" 
+            value={input} 
+            readOnly 
+            style={styles.input} 
+          />
+          <div style={styles.buttons}>
+            {['1', '2', '3', '+', '4', '5', '6', '-', '7', '8', '9', '*', '0', '=', 'C', '/'].map((btn) => (
+              <button 
+                key={btn} 
+                onClick={() => btn === '=' ? handleCalculate() : btn === 'C' ? handleClear() : handleButtonClick(btn)} 
+                style={styles.button}
+              >
+                {btn}
+              </button>
+            ))}
           </div>
         </div>
       </Draggable>
-    </div>
+    )
   );
+}
+
+const styles = {
+  calculator: {
+    width: '90%',
+    maxWidth: '300px',
+    margin: 'auto',
+    padding: '20px',
+    border: '1px solid #ccc',
+    borderRadius: '10px',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+    position: 'absolute', 
+    top: '40%',
+    left: '50%',
+    transform: 'translateX(-50%)',
+    backgroundColor: '#fff',
+    zIndex: 1000,
+  },
+  input: {
+    width: '100%',
+    height: '40px',
+    textAlign: 'right',
+    marginBottom: '10px',
+    fontSize: '18px',
+    borderRadius: '5px',
+    border: '1px solid #ccc',
+    padding: '5px',
+  },
+  buttons: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(4, 1fr)',
+    gap: '10px',
+  },
+  button: {
+    padding: '15px',
+    fontSize: '18px',
+    cursor: 'pointer',
+    borderRadius: '5px',
+    border: '1px solid #ccc',
+    backgroundColor: '#f0f0f0',
+    transition: 'background-color 0.3s',
+  },
+  closeButton: {
+    position: 'absolute',
+    top: '0px',
+    right: '7px',
+    cursor: 'pointer',
+    fontSize: '18px',
+    background: 'transparent',
+    border: 'none',
+    color: '#888',
+    fontWeight: 'bold',
+  }
 };
 
 export default Calculator;
